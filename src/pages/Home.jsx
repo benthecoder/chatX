b426acbbaffcc8fdd04b21b57750b750e02cfcec
benthecoder <benthecoder07@gmail.com>
2022-03-12 16:23:02 -0600

    pages and comp folders

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button';
+
+function Home() {
+  return (
+    <>
+      <div className='Home'>
+        <Button variant='contained'>Text</Button>
+      </div>
+    </>
+  );
+}
+
+export default Home;
