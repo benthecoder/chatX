d53c6a7d486995f077cd346bfe0950dc3d860079
vincent <vincent676lee@gmail.com>
2022-03-12 19:02:07 -0600

    update

--- src/component/App.js
+++ src/component/App.js
@@ -1,22 +1,44 @@
 import "./App.css";
 import { authentication } from "./appFirebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({});
+
   const signInWithGmail = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(authentication, provider)
-      .then((re) => {
-        console.log(re);
+    const auth = authentication;
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        setUser(result.user);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  const signOutGmail = () => {
+    signOut(authentication)
+      .then(() => {
+        const user = document.querySelector(".myclass");
+        user.parentNode.removeChild(user);
+        console.log("Signed out");
+      })
+      .catch((error) => {
+        console.log(error);
       });
   };
 
   return (
     <div className="App">
-      <button onClick={signInWithGmail}>Sign in with google</button>
+      <button onClick={signInWithGmail}>Sign in with google</button> <br />
+      {user.email && (
+        <div className="myclass">
+          <h2>Welcome {user.displayName}</h2>
+          <button onClick={signOutGmail}>Sign Out</button>
+        </div>
+      )}
     </div>
   );
 }
